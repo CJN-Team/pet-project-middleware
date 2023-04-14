cc0bae5e55b94c1966e40b45472235db8d723d8f
DDuQ <estebanduque2@gmail.com>
2023-04-13 19:48:02 -0500

    feat(extensions): Add extensions to register and use the middleware.

---
+++ Pet.Project.Middleware/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Pet.Project.Middleware.Middlewares;
+
+namespace Pet.Project.Middleware.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseGlobalMiddleware(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<GlobalMiddleware>();
+    }
+}
---
+++ Pet.Project.Middleware/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Pet.Project.Middleware.Middlewares;
+
+namespace Pet.Project.Middleware.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddGlobalMiddleware(this IServiceCollection services) 
+    {
+        return services.AddScoped<GlobalMiddleware>();
+    }
+}
